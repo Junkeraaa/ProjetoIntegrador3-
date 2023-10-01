f378c9f390b34f492ab8971d02446b00ec0ff4e9
gusta <gustavobeniciosantos@gmail.com>
2023-10-01 04:15:33 -0300

    Conexão Back e Front com Maven TomCat

---
+++ src/main/java/br/com/suutz/dao/RegisterDAO.java
@@ -0,0 +1,74 @@
+package br.com.suutz.dao;
+
+import br.com.suutz.model.User;
+
+import java.sql.*;
+
+
+public class RegisterDAO {
+
+    public boolean registerUserDAO(User user) {
+        String SQLCheckUser = "SELECT COUNT(*) FROM USUARIO WHERE login=?";
+        String SQLInsertUser = "INSERT INTO USUARIO (login, senha) VALUES (?, ?)";
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+            System.out.println("Connection Success");
+
+            PreparedStatement checkUserStatement = connection.prepareStatement(SQLCheckUser);
+            checkUserStatement.setString(1, user.getUser());
+            ResultSet resultSet = checkUserStatement.executeQuery();
+            resultSet.next();
+            int count = resultSet.getInt(1);
+
+            if (count > 0) {
+                // Usuário já existe, não é possível registrar novamente
+                System.out.println("User already exists. Registration failed.");
+                connection.close();
+                return false;
+            }
+
+
+            // Verifique se a senha tem pelo menos 6 caracteres
+            if (user.getPassword().length() < 6) {
+                System.out.println("Password must have at least 6 characters. Registration failed.");
+                connection.close();
+                return false;
+            }
+
+            // O usuário não existe e a senha tem pelo menos 6 caracteres, pode ser registrado
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLInsertUser);
+            preparedStatement.setString(1, user.getUser());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.execute();
+
+            System.out.println("Success in insert command");
+            connection.close();
+
+            return true; // Registro bem-sucedido
+        } catch (Exception e) {
+            System.out.println("Connection failed");
+            return false; // Registro falhou devido a erro de conexão
+        }
+    }
+
+    public boolean userExists(Connection connection, String SQLCheckUser, User user ) throws SQLException {
+        PreparedStatement checkUserStatement = connection.prepareStatement(SQLCheckUser);
+        checkUserStatement.setString(1, user.getUser());
+        ResultSet resultSet = checkUserStatement.executeQuery();
+        resultSet.next();
+        int count = resultSet.getInt(1);
+
+        if (count > 0) {
+            // Usuário já existe, não é possível registrar novamente
+            System.out.println("User already exists. Registration failed.");
+            connection.close();
+            return false;
+        }
+        return true;
+    }
+}
+
+
+
+
---
+++ src/main/java/br/com/suutz/servlet/RegisterCustomerServlet.java
@@ -0,0 +1,28 @@
+package br.com.suutz.servlet;
+
+import br.com.suutz.dao.RegisterDAO;
+import br.com.suutz.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class RegisterCustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        System.out.println(username+" "+password);
+        User user = new User(username, password);
+        new RegisterDAO().registerUserDAO(user);
+        resp.sendRedirect("index.html");
+}
+    }

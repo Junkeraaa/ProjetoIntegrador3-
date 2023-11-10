b0fae5bb4977c7c7897e8b927ba9b5650f7fff45
LUCAS JUNKER NARVAIS <lucas.jnarvais@senacsp.edu.br>
2023-11-10 20:43:04 -0300

    Subida da estrutura sql e conexao com paginas logadas

--- docs/DDL.sql
+++ docs/DDL.sql
@@ -7,24 +7,37 @@ balance DECIMAL(10,2) NOT NULL
 
 --TB Renda variável
 CREATE TABLE STOCKS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,  -- Chave estrangeira para a tabela de usuários
+   id INT AUTO_INCREMENT PRIMARY KEY,
     name_stock VARCHAR(255) NOT NULL,--nome da ação
+    price_stock DECIMAL(10, 2) NOT NULL--preço/ação
+); 
+
+CREATE TABLE STOCKS_CLIENT (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,  -- Chave estrangeira para a tabela de usuários
+    stock_id INT NOT NULL,--nome da ação
     qtd INT NOT NULL,--quantidade
-    price_stock DECIMAL(10, 2) NOT NULL,--preço/ação
-    FOREIGN KEY (user_id) REFERENCES USUARIOS(id)
-);
+    FOREIGN KEY (user_id) REFERENCES USUARIOS(id),
+    FOREIGN KEY (stock_id) REFERENCES STOCKS(id)
+); 
 
 --TB Renda Fixa
 CREATE TABLE FIXED_INCOME (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,  -- Chave estrangeira para a tabela de usuários
+    price DECIMAL(5, 2) NOT NULL, --peço ação
     name VARCHAR(255) NOT NULL,--nome
     type VARCHAR(255) NOT NULL,--tipo
-    fee DECIMAL(5, 2) NOT NULL,--taxa
+    fee DECIMAL(5, 2) NOT NULL--taxa
+);
+
+CREATE TABLE FIXED_INCOME_CLIENT (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,  -- Chave estrangeira para a tabela de usuários
+    fixed_income_id INT NOT NULL, --Chave estrangeira para associar a renda fixa
     amount DECIMAL(10, 2) NOT NULL,--montante
     yeld DECIMAL(10, 2) NOT NULL,--rendimento
-    FOREIGN KEY (user_id) REFERENCES USUARIOS(id)
+    FOREIGN KEY (user_id) REFERENCES USUARIOS(id),
+    FOREIGN KEY (fixed_income_id) REFERENCES FIXED_INCOME(id)
 );
 
 
